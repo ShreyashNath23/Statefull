9dd343194762e26c520c108d0e0168c0ce2c7892
Shreyash Nath <shrey7468@gmail.com>
2025-02-05 13:11:59 +0530

    In .env url changed to claimmgmtdb to claimmgmt

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/policy", policyroute);
 app.use("/policyholder", policyholderroute);
 app.use("/claim", claimroute);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
--- db.js
+++ db.js
@@ -1,15 +1,13 @@
-// const { Pool } = require("pg");
-// require("dotenv").config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-//   // connectionString: process.env.DB_URL,
-//   // ssl: { rejectUnauthorized: false },
-// });
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 // // module.exports = pool;
 
@@ -21,14 +19,14 @@
 // //   ssl: { rejectUnauthorized: false },
 // // });
 
-// module.exports = pool;
-
-const { Pool } = require("pg");
-require("dotenv").config();
-
-const pool = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: { rejectUnauthorized: false }, // Required for Render PostgreSQL
-});
-
 module.exports = pool;
+
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   connectionString: process.env.DB_URL,
+//   ssl: { rejectUnauthorized: false }, // Required for Render PostgreSQL
+// });
+
+// module.exports = pool;
