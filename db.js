41ef00eb1244e586c964c2b9a6c654a079d8e024
Shreyash Nath <shrey7468@gmail.com>
2025-02-05 15:04:43 +0530

    Change in db code

--- db.js
+++ db.js
@@ -1,25 +1,38 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-
-// // module.exports = pool;
-
-// // const { Pool } = require("pg");
-// // require("dotenv").config();
-
-// // const pool = new Pool({
-// //   connectionString: process.env.DB_URL,
-// //   ssl: { rejectUnauthorized: false },
-// // });
+let pool;
+if (process.env.DB_URL) {
+  // When deployed on Render, use the provided DB_URL and add SSL settings.
+  pool = new Pool({
+    connectionString: process.env.DB_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+} else {
+  // When running locally, use the local environment variables.
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
 module.exports = pool;
+
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+// module.exports = pool;
 
 // const { Pool } = require("pg");
 // require("dotenv").config();
